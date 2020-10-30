75fd80a1f2f12c2f9babe72174b462483e0e19b6
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2020-10-30 17:51:26 +0900

    コメント位置変更

--- apply_style.js
+++ apply_style.js
@@ -6,14 +6,18 @@ function append_css(file_path) {
     link.type = "text/css";
     head.appendChild(link);
 }
+
 // append_css("css/constant.css");
+
 // 文字サイズ・背景色・フォントをsyncストレージから取得する
 // undefinedならデフォルト値が代入される
+
+// 表示調整ボタン
 let disp, size, bgcolor, font;
 browser.storage.sync.get(["display_button"], function (res) {
+    
     let rd = res.display_button;
     disp = (rd != undefined) ? rd : "hide"; // display, hide
-    // // 表示調整ボタン
     // const disp_dir = "./css/button/";
     // let disp_css;
     // switch (disp) {
@@ -26,10 +30,11 @@ browser.storage.sync.get(["display_button"], function (res) {
     // }
     // append_css(disp_css);
 });
+
+// 文字サイズ
 browser.storage.sync.get(["font_size"], function (res) {
     let rs = res.font_size;
     size = (rs != undefined) ? rs : "middle"; // small, middle, large, huge
-    // // 文字サイズ
     // const size_dir = "./css/size/";
     // let size_css;
     // switch (size) {
@@ -44,19 +49,20 @@ browser.storage.sync.get(["font_size"], function (res) {
     // }
     // append_css(size_css);
 });
+
+// 背景色
 browser.storage.sync.get(["background_color"], function (res) {
     let rb = res.background_color;
     bgcolor = (rb != undefined) ? rb : "white"; // white, black, kinari, blue
-    // 背景色
     const bgcolor_dir = "css/color/";
     let bgcolor_css = bgcolor_dir + bgcolor + ".css";
     append_css(bgcolor_css);
     append_css("css/finalize.css");
 });
 browser.storage.sync.get(["font_style"], function (res) {
+    // フォント
     let rf = res.font_style;
     font = (rf != undefined) ? rf : "mincho"; // mincho, gothic
-    // // フォント
     // const font_dir = "./css/font/"
     // let font_css;
     // switch (font) {
