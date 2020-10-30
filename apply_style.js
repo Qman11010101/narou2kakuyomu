79b39f76f1a897c47853cab6840c3475736abead
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2020-10-30 19:13:42 +0900

    完成

--- apply_style.js
+++ apply_style.js
@@ -7,7 +7,6 @@ function append_css(file_path) {
     head.appendChild(link);
 }
 
-// append_css("css/constant.css");
 
 // 文字サイズ・背景色・フォントをsyncストレージから取得する
 // undefinedならデフォルト値が代入される
@@ -15,61 +14,44 @@ function append_css(file_path) {
 // 表示調整ボタン
 let disp, size, bgcolor, font;
 browser.storage.sync.get(["display_button"], function (res) {
-    
-    let rd = res.display_button;
+    const rd = res.display_button;
     disp = (rd != undefined) ? rd : "hide"; // display, hide
-    // const disp_dir = "./css/button/";
-    // let disp_css;
-    // switch (disp) {
-    //     case "display":
-    //         disp_css = disp_dir + "display.css";
-    //     case "hide":
-    //         disp_css = disp_dir + "hide.css";
-    //     default:
-    //         disp_css = disp_dir + "hide.css";
-    // }
-    // append_css(disp_css);
+
+    // 1要素だけなのでCSSファイル適用しないで直接弄っている
+    let toggle = document.getElementsByClassName("toggle");
+    if (disp == "hide"){
+        toggle[0].style.display = "none";
+    } else {
+        toggle[0].style.display = "inline";
+    }
 });
 
 // 文字サイズ
 browser.storage.sync.get(["font_size"], function (res) {
-    let rs = res.font_size;
+    const rs = res.font_size;
     size = (rs != undefined) ? rs : "middle"; // small, middle, large, huge
-    // const size_dir = "./css/size/";
-    // let size_css;
-    // switch (size) {
-    //     case "small":
-    //         size_css = size_dir + "small.css";
-    //     case "middle":
-    //         size_css = size_dir + "middle.css";
-    //     case "large":
-    //         size_css = size_dir + "large.css";
-    //     case "huge":
-    //         size_css = size_dir + "huge.css";
-    // }
-    // append_css(size_css);
+    const size_dir = "./css/size/";
+    const size_css = size_dir + size + ".css";
+    append_css(size_css);
+    append_css(size_dir + "apply_size.css");
 });
 
 // 背景色
 browser.storage.sync.get(["background_color"], function (res) {
-    let rb = res.background_color;
+    const rb = res.background_color;
     bgcolor = (rb != undefined) ? rb : "white"; // white, black, kinari, blue
     const bgcolor_dir = "css/color/";
-    let bgcolor_css = bgcolor_dir + bgcolor + ".css";
+    const bgcolor_css = bgcolor_dir + bgcolor + ".css";
     append_css(bgcolor_css);
-    append_css("css/finalize.css");
+    append_css(bgcolor_dir + "apply_color.css");
 });
+
+// フォント
 browser.storage.sync.get(["font_style"], function (res) {
-    // フォント
-    let rf = res.font_style;
+    const rf = res.font_style;
     font = (rf != undefined) ? rf : "mincho"; // mincho, gothic
-    // const font_dir = "./css/font/"
-    // let font_css;
-    // switch (font) {
-    //     case "mincho":
-    //         font_css = font_dir + "mincho.css";
-    //     case "gothic":
-    //         font_css = font_dir + "gothic.css";
-    // }
-    // append_css(font_css);
+    const font_dir = "css/font/"
+    const font_css = font_dir + font + ".css";
+    append_css(font_css);
+    append_css(font_dir + "apply_font.css");
 });
