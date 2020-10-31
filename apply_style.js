8b4880b2258bfa73f61ca77a8849e088da5869da
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2020-10-31 21:24:20 +0900

    Update apply_style.js

--- apply_style.js
+++ apply_style.js
@@ -10,7 +10,7 @@ function append_css(file_path) {
 // 目次/前へ/次へボタンにID付与していじりやすくする
 let novel_bn = document.querySelectorAll(".novel_bn a");
 const id_list = ["upper_back", "upper_next", "lower_back", "lower_next", "table_contents"]
-for (let i=0; i<novel_bn.length; i++){
+for (let i = 0; i < novel_bn.length; i++) {
     novel_bn[i].id = id_list[i];
 }
 
@@ -25,7 +25,7 @@ browser.storage.sync.get(["display_button"], function (res) {
 
     // 1要素だけなのでCSSファイル適用しないで直接弄っている
     let toggle = document.getElementsByClassName("toggle");
-    if (disp == "hide"){
+    if (disp === "hide") {
         toggle[0].style.display = "none";
     } else {
         toggle[0].style.display = "inline";
@@ -35,7 +35,7 @@ browser.storage.sync.get(["display_button"], function (res) {
 // 文字サイズ
 browser.storage.sync.get(["font_size"], function (res) {
     const rs = res.font_size;
-    size = (rs != undefined) ? rs : "medium"; // small, medium, large, huge
+    size = (rs === undefined) ? "medium" : rs; // small, medium, large, huge
     const size_dir = "./css/size/";
     const size_css = size_dir + size + ".css";
     append_css(size_css);
@@ -45,7 +45,7 @@ browser.storage.sync.get(["font_size"], function (res) {
 // 背景色
 browser.storage.sync.get(["background_color"], function (res) {
     const rb = res.background_color;
-    bgcolor = (rb != undefined) ? rb : "white"; // white, black, kinari, blue
+    bgcolor = (rb === undefined) ? "white" : rb; // white, black, kinari, blue
     const bgcolor_dir = "css/color/";
     const bgcolor_css = bgcolor_dir + bgcolor + ".css";
     append_css(bgcolor_css);
@@ -55,7 +55,7 @@ browser.storage.sync.get(["background_color"], function (res) {
 // フォント
 browser.storage.sync.get(["font_style"], function (res) {
     const rf = res.font_style;
-    font = (rf != undefined) ? rf : "mincho"; // mincho, gothic
+    font = (rf === undefined) ? "mincho" : rf; // mincho, gothic
     const font_dir = "css/font/"
     const font_css = font_dir + font + ".css";
     append_css(font_css);
