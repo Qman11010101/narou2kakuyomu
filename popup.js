f6276e7728986e2ca6c54859d9c6638d1e166dcb
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2020-12-03 01:23:17 +0900

    rename some files

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // データ保存
     const save_data = document.getElementById("save_data");
     save_data.addEventListener("click", function () {
         const disp = document.getElementById("display_button").display_button.value;
@@ -15,6 +16,46 @@ document.addEventListener("DOMContentLoaded", function () {
         };
         browser.storage.sync.set(kvobj, check_pass());
     });
+
+    //データ読込
+    const load_data = document.getElementById("load_data");
+    load_data.addEventListener("click", function () {
+        const disp_elements = document.getElementById("display_button").display_button;
+        const scri_elements = document.getElementById("display_pref_post").display_pref_post;
+        const size_elements = document.getElementById("font_size").font_size;
+        const bgcl_elements = document.getElementById("background_color").background_color;
+        const font_elements = document.getElementById("font_style").font_style;
+
+        let disp, scri, size, bgcl, font;
+        browser.storage.sync.get(["display_button"], function (res) {
+            const rd = res.display_button;
+            disp = (rd != undefined) ? rd : "hide"; // display, hide
+            for (let i = 0; i < disp_elements.length; i++) {
+                //alert(disp_elements[i].value);
+            }
+        });
+        browser.storage.sync.get(["display_pref_post"], function (res) {
+            const rds = res.display_pref_post;
+            scri = (rds != undefined) ? rds : "hide"; // display, hide
+
+        });
+        browser.storage.sync.get(["font_size"], function (res) {
+            const rs = res.font_size;
+            size = (rs === undefined) ? "medium" : rs; // small, medium, large, huge
+
+        });
+        browser.storage.sync.get(["background_color"], function (res) {
+            const rb = res.background_color;
+            bgcl = (rb === undefined) ? "white" : rb; // white, black, kinari, blue
+
+        });
+        browser.storage.sync.get(["font_style"], function (res) {
+            const rf = res.font_style;
+            font = (rf === undefined) ? "mincho" : rf; // mincho, gothic
+
+        });
+    });
+    // フォント変更適用
     const gothicdiv = document.getElementById("gothicdiv");
     gothicdiv.addEventListener("click", function () {
         const label = document.getElementsByTagName("label");
